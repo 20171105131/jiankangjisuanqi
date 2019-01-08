8c28025c5b42bc68e2cf94a2f124922eba98633e
20171105131 <1105197998@qq.com>
2019-01-08 10:37:18 +0800

    增加归零

--- jiankangjisuanqi/ViewController.swift
+++ jiankangjisuanqi/ViewController.swift
@@ -17,40 +17,38 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var TEXT2: UITextField!
     
-    @IBAction func Clear(_ sender: Any) {
-        if frog == 1
+    @IBAction func Enter(_ sender: Any) {
+        if frog == 0
+        {
+            sum = Int(TEXT1.text!)!
+            TEXT1.text = ""
+            frog = 1
+        }
+        else if frog == 1
         {
             sums = Int(TEXT1.text!)!
             sumb = sums / (( sum / 100 )^2)
             TEXT1.text = "\(sumb)"
             frog = 2
         }
-        else if frog == 0
-        {
-            sum = Int(TEXT1.text!)!
-            TEXT1.text = ""
-            TEXT2.text = ""
-            TEXT5.text = ""
-            frog = 1
-        }
         if frog == 2
         {
-            if sumb < Int(18.4)
+            if sumb <= Int(18.4)
             {
                 TEXT2.text = "偏瘦"
                 TEXT5.text = "注意饮食控制以及加强体力活动和锻炼。"
             }
-            if sumb > Int(18.5) && sumb < Int(23.9)
+            if sumb >= Int(18.5) && sumb <= Int(23.9)
             {
                 TEXT2.text = "正常"
                 TEXT5.text = "继续保持。"
             }
-            if sumb > Int(24) && sumb < Int(27.9)
+            if sumb >= Int(24) && sumb <= Int(27.9)
             {
                 TEXT2.text = "过重"
                 TEXT5.text = "增加饮食中蔬菜和水以及以及低热量饮食。"
             }
-            if sumb > Int(28)
+            if sumb >= Int(28)
             {
                 TEXT2.text = "肥胖"
                 TEXT5.text = "在合理饮食的基础上，增加体力活动是减轻体重的关键。体育运动的安排应当根据你的情况个性化安排，从小运动量、短时间开始逐渐加大运动量。"
@@ -58,10 +56,17 @@ class ViewController: UIViewController {
             frog = 0
             
         }
-        
+    
         
     }
     
+    @IBAction func Clear(_ sender: Any)
+    {
+        TEXT1.text = ""
+        TEXT2.text = ""
+        TEXT5.text = ""
+        frog = 0
+    }
     @IBOutlet var TEXT4: [UITextView]!
     
     @IBOutlet weak var TEXT5: UITextView!
